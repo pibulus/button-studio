1f157a9ef01f371d0b00cb986310da59ead0c149
Pablo Alvarado <pibulus@gmail.com>
2025-08-15 20:33:03 +0700

    feat: 🧪 Added Button Physics Lab with 9 different button feelings
    Created testing page with button variants:
    - Snappy: Ultra-fast 50ms response
    - Gummy: Slow sticky 300ms press
    - Bouncy: Exaggerated spring physics
    - Heavy: Feels weighty with lift on hover
    - Soft: Cushioned with soft shadows
    - Clicky: Binary mechanical switch (20ms)
    - Wobbly: Jiggle rotation physics
    - Magnetic: Grows on hover, snaps down
    
    Each variant tweaks transition timing, travel distance, and animation curves to create distinct tactile feelings

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $button_lab from "./routes/button-lab.tsx";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $squishy_demo from "./routes/squishy-demo.tsx";
@@ -16,6 +17,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/button-lab.tsx": $button_lab,
     "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
     "./routes/squishy-demo.tsx": $squishy_demo,
